9a8d1c8f73433ad015ac1698553d1a5c7510a9a5
Filip Jankowski <nicnaniby>
2024-11-02 16:04:09 +0100

    Added the project based on tutorial

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Welcome(string name, int numTimes = 1)
+    {
+        var model = new MyModel();
+        model.NumTimes = numTimes;
+
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
+    }
+}
+
+public class MyModel
+{
+    public int NumTimes { get; set; }
+}
